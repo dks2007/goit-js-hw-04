3466732e0ccb4ad57f2c95ff1b61b4223eb64c20
Dima Karpinskyi <dimakjkarpinski@gmail.com>
2025-11-23 16:09:28 +0100

    hm

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,28 @@
-function slugify(title) {
-    const slug = title
-        .toLowerCase() //!SECTION переводим в нижній регістр
-        .split(' ') //!SECTION  розбиваємо рядок на масив слів
-        .join('-'); //!SECTION  об'єднуємо масив слів у рядок, розділяючи їх дефісами
-    return slug;
+function isEnoughCapacity(products, containerSize) {
+    let total = 0;
+
+    for (const key in products) {
+        const qty = products[key];
+        total += qty;
+
+        if (total > containerSize) {
+            return false;
+        }
+    }
+    return true;
 }
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
